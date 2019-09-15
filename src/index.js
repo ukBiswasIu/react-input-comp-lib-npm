61285808e51b970c1dc5bcc2c13c3864abac9e3c
Syncrhonous <syncmac@Syncrhonouss-Mac-mini.local>
2019-09-15 13:40:47 +0600

    changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ class InputBox extends React.Component {
   render() {
     return (
       <div>
-        <span style={"font-size: " + this.props.label-font-size + ";"}>{this.props.label + ": " || ""}</span>
+        <span>{this.props.label + ": " || ""}</span>
         <input
           className={this.props.className}
           type={this.props.type || "text"}
